63aef6d9febcc98d7825fe4f3ac18971a91c6db6
antquu <ant.adam468@gmail.com>
2026-04-16 14:31:50 +0200

    filtre appareils non reconnu sur le site

---
+++ src/components/nomobile.jsx
@@ -0,0 +1,19 @@
+function NoMobile() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      width: '100vw',
+    }}>
+      <img 
+        src="/nomobile.png" 
+        alt="Mobile uniquement"
+        style={{ maxWidth: '400px', width: '80%' }}
+      />
+    </div>
+  )
+}
+
+export default NoMobile
