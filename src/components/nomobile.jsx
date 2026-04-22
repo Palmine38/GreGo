07fb7ac9407866a8bf06eeb820238f3363e23f5a
Palmine <106870314+Palmine38@users.noreply.github.com>
2026-04-22 17:37:02 +0200

    Resolve merge conflict in NoMobile component

--- src/components/nomobile.jsx
+++ src/components/nomobile.jsx
@@ -1,21 +1,4 @@
 function NoMobile() {
-<<<<<<< HEAD
-    return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100vw',
-        }}>
-            <img
-                src="/nomobile.png"
-                alt="Mobile uniquement"
-                style={{ maxWidth: '400px', width: '80%' }}
-            />
-        </div>
-    )
-=======
   return (
     <div style={{
       display: 'flex',
@@ -31,7 +14,6 @@ function NoMobile() {
       />
     </div>
   )
->>>>>>> e14dd44741f560a59c2b3a7b1eda54f23ab6822b
 }
 
 export default NoMobile
